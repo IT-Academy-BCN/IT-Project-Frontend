8b8cfc0cdad39f6dce23a34c65a9b01cd23edd90
Cristian Fàbregas <cristian.fabregas@gmil.com>
2020-03-13 12:41:44 +0100

    Primera versión funcional de la aplicación a falta de Student

--- src/app/Views/reviews/reviews-routing.module.ts
+++ src/app/Views/reviews/reviews-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+/* own */
+import { ReviewsViewComponent } from './reviews-view/reviews-view.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'reviews-view',  component: ReviewsViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+/* components */
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { LoginViewComponent } from './Views/login/login-view/login-view.component';
+import { ResetPasswordRequestComponent } from './Views/login/reset-password-request/reset-password-request.component';
+
+//import {ClassroomModule } from './Views/classroom/classroom.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginViewComponent },
+  { path: 'login/reset-password', component: ResetPasswordRequestComponent },
+  {
+    path:'header',
+    component:HeaderComponent
+  },
+  {
+    path:'footer',
+    component:FooterComponent
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'login' }
+]
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
