7fa69d3fed0edb609ee4595b08dd6b6dcf88ca7b
Ana Pascual <annapascual001@gmail.com>
2020-03-30 10:41:22 +0200

    Renaming statistics-pies folder

--- src/app/Views/statistics/statistics.module.ts
+++ src/app/Views/statistics/statistics.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ChartsModule } from 'ng2-charts';
 /* components & modules */
 import { TablesComponent} from './tables/tables.component'
-import { GenrepieComponent } from './stadistics/genrepie/genrepie.component';
+import { GenrepieComponent } from './statistics-pies/genrepie/genrepie.component';
 import { StatisticsRoutingModule } from './statistics-routing.module';
-import { PeoplepieComponent } from './stadistics/peoplepie/peoplepie.component';
+import { PeoplepieComponent } from './statistics-pies/peoplepie/peoplepie.component';
 import { StatisticsViewComponent } from './statistics-view/statistics-view.component'
 
 @NgModule({
