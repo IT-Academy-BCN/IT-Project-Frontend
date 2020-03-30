50fda4f6792a9115ec5214601bff8e5bd466b894
Ana Pascual <annapascual001@gmail.com>
2020-03-30 10:32:34 +0200

    Removing duplicated component

--- src/app/Views/statistics/statistics.module.ts
+++ src/app/Views/statistics/statistics.module.ts
@@ -5,12 +5,11 @@ import { ChartsModule } from 'ng2-charts';
 import { TablesComponent} from './tables/tables.component'
 import { GenrepieComponent } from './stadistics/genrepie/genrepie.component';
 import { StatisticsRoutingModule } from './statistics-routing.module';
-import { StadisticsComponent } from './stadistics/stadistics.component';
 import { PeoplepieComponent } from './stadistics/peoplepie/peoplepie.component';
 import { StatisticsViewComponent } from './statistics-view/statistics-view.component'
 
 @NgModule({
-  declarations: [ StadisticsComponent, PeoplepieComponent, GenrepieComponent,TablesComponent, StatisticsViewComponent],
+  declarations: [ PeoplepieComponent, GenrepieComponent,TablesComponent, StatisticsViewComponent],
   imports: [
     CommonModule,
     ChartsModule,
