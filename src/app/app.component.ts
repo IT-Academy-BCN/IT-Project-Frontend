3d5da443277a515c80375bf2856f13d72b122f11
unknown <miquelmtllns@pm.me>
2020-03-30 15:03:28 +0200

    Header Layout
    Changes on app.component
    
    Add logic to hide header in login view and login reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularTest';
+  showHeader = false; // if we change it to true, it works correctly, but if the user performs a debugger from the browser, it can stop loading just when the menu is displayed. Better to initialize to false.
+
+  router:any;
+  constructor( private _router: Router ) {
+    this.router = _router;
+  }
+  ngOnInit() {
+
+  }
+  modifyHeader() { // This method is called many times
+debugger
+    console.log(this.router.url); // This prints a loot of routes on console
+    if (this.router.url == '/login' || this.router.url == '/login/reset-password') {
+      this.showHeader = false;
+    } else {
+      this.showHeader = true;
+    }
+  }
 }
+
+
+
