59c51a0bbc4866281e1aa08f19972f526ef53c80
Darío Scattolini <dario.scattolini@gmail.com>
2020-04-15 17:22:15 +0200

    refactor <student-view>: verify reception of url parameters

--- src/app/Views/student-file/student-file-view/student-file-view.ts
+++ src/app/Views/student-file/student-file-view/student-file-view.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
+/* to receive id parameter from route */
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
 /* services */
 import { ExerciseService } from '../../../Services/exercise.service';
 import { StudentSearchService } from '../../../Services/student-search.service';
@@ -13,12 +18,26 @@ import { StudentSearchService } from '../../../Services/student-search.service';
 })
 export class StudentFileViewComponent implements OnInit {
 
+    public student;
 
-  constructor(private exerciseService: ExerciseService, private search: StudentSearchService) {
+  constructor(
+    private exerciseService: ExerciseService,
+    private search: StudentSearchService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {
     search.page = 'students';
   }
 
   ngOnInit() {
+    this.student = this.route.snapshot.params.id;
+
+    /* if student service returned an observable, we could do something like this:
+    this.student = this.route.paramMap.pipe(
+      switchMap(
+        (params: ParamMap) => this.studentService.getStudent(params.get('id'))
+      )
+    ); */
   }
 
 }
