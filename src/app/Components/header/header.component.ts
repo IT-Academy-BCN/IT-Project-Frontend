69bf1ae3efd039041eb14fbb57a77ba79d3c0e60
AnastasiaGulyaeva <nasty-n@mail.ru>
2020-04-20 10:54:57 +0200

    Header-footer-logout

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,29 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import { User } from '../../Models/user.model';
+import { Component, OnInit } from "@angular/core";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { User } from "../../Models/user.model";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
   faSignOutAlt = faSignOutAlt;
-  user: User = new User;
+  user: User = new User();
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  // logout () {    
+  // logout () {
   //   let menu = <HTMLAnchorElement>document.getElementById('menu');
   //   let login = <HTMLAnchorElement>document.getElementById('login');
-    
+
   //   login.classList.remove("d-none");
   //   menu.classList.add("d-none");
-  
+
   //   //TODO: añadir lógica de logout
   // }
 
-  ngOnInit(): void {
+  logout() {
+    this.router.navigateByUrl("/login");
   }
 
+  ngOnInit(): void {}
 }
