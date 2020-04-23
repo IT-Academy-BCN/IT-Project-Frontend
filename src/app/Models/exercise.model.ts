7ee3a84a6c41110812f13a6c1a17876f0a2d99dc
Darío Scattolini <dario.scattolini@gmail.com>
2020-04-23 21:27:12 +0200

    feat <exercise model>: add excercise models and constants

---
+++ src/app/Models/exercise.model.ts
@@ -0,0 +1,61 @@
+  // types and enums below link API statuses and itinerary ids with meaningful names
+  // and prevents hard-coding of constants everywhere else in the code
+  // itinerary ideally belongs to another module
+
+enum ItineraryId {
+  CommonBlock = 1,
+  FrontEnd,
+  BackEndJava,
+  BackEndDotNet
+}
+
+type ItineraryName = keyof typeof ItineraryId;
+
+export const Itineraries: Record<ItineraryName, string> = {
+  CommonBlock: 'Bloque común',
+  FrontEnd: 'Front-end',
+  BackEndJava: 'Back-End - JAVA',
+  BackEndDotNet: 'Back-End - .NET',
+} as const;
+
+enum ExerciseStatusId {
+  None = 1,
+  TurnedIn,
+  Received,
+  Checked,
+  Finished
+}
+
+type ExerciseStatusName = keyof typeof ExerciseStatusId;
+
+export const Statuses: Record<ExerciseStatusName, string> = {
+  None: 'ninguno',
+  TurnedIn: 'entregado',
+  Received: 'recibido - revisión pendiente',
+  Checked: 'revisado - discusión pendiente',
+  Finished: 'finalizado'
+} as const;
+
+interface Itinerary {
+  id: ItineraryId;
+  name: string;
+}
+
+interface Status {
+  name: string;
+  id: ExerciseStatusId;
+  date: Date;
+}
+
+interface Teacher {
+  id: string;
+  name: string;
+}
+
+export class StudentExercise {
+  id: string;
+  name: string;
+  status: Status;
+  itinerary: Itinerary;
+  teacher: Teacher;
+}
