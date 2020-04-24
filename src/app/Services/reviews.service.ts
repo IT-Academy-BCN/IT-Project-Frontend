9e7fa8169828d7af9722a1e3ddbb7526c7a945ac
Hector Pascual <hector.pascual.diaz@gmail.com>
2020-04-24 13:36:14 +0200

    Table filled with api call on review module.
    First approach to the solution, not efficient and neat solution.

--- src/app/Services/reviews.service.ts
+++ src/app/Services/reviews.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+// import { StudentReview } from '../Models/studentsReview';
+import { StudentReview, Exercices } from '../Models/exercises.model';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { StudentInList } from '../Models/exercises.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewsService {
 
-  constructor() { }
+
+  private apiExercises = 'http://217.76.158.200:8090/api/exercises/';
+  private apiStudents = 'http://217.76.158.200:8090/api/students';
+
+  constructor(private http: HttpClient) { }
+
+  getExercises(){
+    return this.http.get<Exercices[]>(this.apiExercises)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  errorHandler(error:HttpErrorResponse){
+    return Observable.throw(error.message || "Server Error");
+  }
+
+  getAllStudents() {
+    return this.http.get<StudentInList[]>(this.apiStudents)
+      .pipe(
+        catchError(this.errorHandler)
+      );
+  }
 }
--- src/app/Views/reviews/table/table.component.ts
+++ src/app/Views/reviews/table/table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentReview } from '../../../Models/studentsReview';
 import { ReviewStudentsService } from '../../../Services/review-students.service';
+import { ReviewsService } from '../../../Services/reviews.service';
+import { Exercices, StudentInList, Students } from '../../../Models/exercises.model';
 
 @Component({
   selector: 'app-table',
@@ -11,17 +13,94 @@ export class TableComponent implements OnInit {
 
   selectedPage: number = 1;
   students: StudentReview[] = [];
-  // rows: number;
-  // cols: number;
 
-  constructor(private reviewStudents: ReviewStudentsService) {
+  public ExerciseElement = {} as Students;
+  public allExercises: any = [];
+  public ExercicesArray: any = [];
+  public StudentsArray: any = [];
+
+  constructor(private reviewStudents: ReviewsService, private studentService: ReviewsService) {
   }
 
   ngOnInit() {
-    this.students = this.reviewStudents.getStudentsReview();
-    console.log(this.reviewStudents); // fake json in console
-    // this.cols = this.students[0].exercises.length;
-    // this.rows = this.students.length;
+    this.fetchStudentsData();
+  }
+
+  fetchStudentsData() {
+    return this.studentService
+      .getAllStudents()
+      .subscribe(this.fillStudentsData);
+  }
+
+  fetchExercicesData() {
+    return this.reviewStudents
+      .getExercises()
+      .subscribe(this.fillExercicesData);
+  }
+
+  fillStudentsData = (students: StudentInList[]) => {
+    this.StudentsArray = students;
+    this.fetchExercicesData();
+    return this.StudentsArray;
+  }
+
+  fillExercicesData = (exercices: Exercices[]) => {
+    for (let key in exercices) {
+      this.ExercicesArray.push(exercices[key]);
+    }
+    this.ExercicesReadyToDisplay();
+    return this.ExercicesArray;
+  }
+
+  ExercicesReadyToDisplay() {
+    let status: string="No data";
+    let date: Date;
+    let idSubInt: number;
+    
+    // Comprobar que estos son los nombres del itinerario
+    let arrItinerary=["Bloque Común","Front End","Back End",".NET",""];
+    let statusObj={"FINISHED": "Corregido", "No data":"No entregado", "To define1": "Entregado", "To define2":"A revisar", "To define3":""}
+    for (let key in this.StudentsArray) {
+      let arrSubjects: any = [];
+      idSubInt = -1;
+      for (let key2 in this.ExercicesArray) {
+        if (this.StudentsArray[key].id in this.ExercicesArray[key2].students) {
+          for (let key3 in this.ExercicesArray[key2].students) { 
+            if (key3 == this.StudentsArray[key].id) {
+              status = statusObj[this.ExercicesArray[key2].students[key3].statusExercise];
+              date = new Date(this.ExercicesArray[key2].students[key3].date);
+              idSubInt++;
+            }
+            
+          }
+          let subject = {
+            id: idSubInt,
+            name: this.ExercicesArray[key2].name,
+            itinerary: arrItinerary[(this.ExercicesArray[key2].itinerary)-1],
+            state: status,
+            date: date
+          };
+          arrSubjects.push(subject);
+        } else {
+          let subject = {
+            id: -1,
+            name: this.ExercicesArray[key2].name,
+            itinerary: arrItinerary[4],
+            state: "No entregado",
+            date: new Date()
+          };
+          arrSubjects.push(subject);
+        }
+      }
+      this.ExerciseElement = {
+        id: this.StudentsArray[key].id,
+        firstName: this.StudentsArray[key].firstName,
+        lastName: this.StudentsArray[key].lastName,
+        exercises: arrSubjects
+      };
+      this.allExercises.push(this.ExerciseElement);
+    }
+    // console.log(this.allExercises);
   }
 
 }
