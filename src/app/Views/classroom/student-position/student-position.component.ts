8de2bbb61089a27566aa3b8d9e2af04baa550a52
Flor Lopez <f.lopezfrey@gmail.com>
2020-05-05 00:31:04 -0300

    tidy up number-of-itinerary call

--- src/app/Views/classroom/student-position/student-position.component.ts
+++ src/app/Views/classroom/student-position/student-position.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ClassroomService, StudentInfo } from '../../../Services/classroom.service';
+import { ClassroomService } from '../../../Services/classroom.service';
 
 @Component({
   selector: 'app-student-position',
@@ -8,12 +8,21 @@ import { ClassroomService, StudentInfo } from '../../../Services/classroom.servi
 })
 export class StudentPositionComponent implements OnInit {
   @Input() studentFP: any = {};
+  @Input() studentItinerary: any = {};
 
   constructor(private classroomService: ClassroomService) { }
 
   ngOnInit() {
     // añadir el progreso aleatorio del alumno
     this.classroomService.addRandomDaysFP();
+
+    // get the number of itinerary for each student
+    this.itineraryNumber(this.studentFP);
+  }
+
+  itineraryNumber(data) {
+    this.studentItinerary = data.courses[0].itinerary.id;
+    return this.studentItinerary;
   }
 
 }
