f29b253f4ffc77169acb6d114c0b9d07a20d8662
vantello <56789310+vantello@users.noreply.github.com>
2020-05-13 18:45:09 +0200

    Update project-routing.module.ts

--- src/app/Views/project/project-routing.module.ts
+++ src/app/Views/project/project-routing.module.ts
@@ -8,15 +8,6 @@ import { ProjectFileViewComponent } from './project-file-view/project-file-view.
 const routes: Routes = [
   { path: 'project-view',  component: ProjectViewComponent },
   { path: 'project-view/:id', component: ProjectFileViewComponent },
-=======
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ProjectViewComponent } from './project-view/project-view.component';
-
-const routes: Routes = [
-  { path: 'project-view',  component: ProjectViewComponent },
->>>>>>> ce9070964240856d2ae91e67617b4be491e1c4c1
-];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
