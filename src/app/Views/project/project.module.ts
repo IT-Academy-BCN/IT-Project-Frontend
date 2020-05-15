120066845d27b077152610275a95b6df9c6c081b
Aleix  Tello <aleixtello@MBPATM.local>
2020-05-15 12:00:26 +0200

    View Connexions

--- src/app/Views/project/project.module.ts
+++ src/app/Views/project/project.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 /* ngx-bootstrap */
+import { ChartsModule } from 'ng2-charts';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
@@ -9,14 +10,18 @@ import { NgxPaginationModule } from 'ngx-pagination';
 /* modules & components */
 import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectViewComponent } from './project-view/project-view.component';
+import { ProjectFileViewComponent } from './project-file-view/project-file-view.component';
+
 
 
 @NgModule({
   declarations: [
     ProjectViewComponent,
+    ProjectFileViewComponent,
   ],
   imports: [
     CommonModule,
+    ChartsModule,
     BsDatepickerModule.forRoot(),
     ModalModule.forRoot(),
     NgxPaginationModule,
