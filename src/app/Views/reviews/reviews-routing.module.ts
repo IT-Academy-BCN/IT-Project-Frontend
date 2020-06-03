756d94532bf632a9c966361cb119b959365d2770
Dag021 <Dag021@gmail.com>
2020-06-03 10:55:57 +0200

    F-50 Research and implement how to get  Angular User Sessions. - Protect the url from non-logged users with Angular Router Guards (CanActivate/Auth Guard) and Redirect the URL to Login - Created a Login Service for logged users - Correct bug from F-58 Loading Modal when the logged is not correct

--- src/app/Views/reviews/reviews-routing.module.ts
+++ src/app/Views/reviews/reviews-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../../Guards/auth.guard';
 /* own */
 import { ReviewsViewComponent } from './reviews-view/reviews-view.component';
 
 const routes: Routes = [
-  { path: 'reviews-view',  component: ReviewsViewComponent },
+  { path: 'reviews-view',  component: ReviewsViewComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
--- src/app/Views/statistics/statistics-routing.module.ts
+++ src/app/Views/statistics/statistics-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../../Guards/auth.guard';
 /* own */
 import { StatisticsViewComponent } from './statistics-view/statistics-view.component';
 
 
 const routes: Routes = [
-  { path: 'statistics-view',  component: StatisticsViewComponent },
+  { path: 'statistics-view',  component: StatisticsViewComponent, canActivate: [AuthGuard] },
 
 ];
 
