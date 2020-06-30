26950a55140009b2b94099cc3f646d240bf89239
DanielMin2009 <daniel.minguella.blasco@gmail.com>
2020-06-30 16:17:27 +0200

    condition in forEach to exclude that we don't need

--- src/app/Views/statistics/statistics-pies/peoplepie/peoplepie.component.ts
+++ src/app/Views/statistics/statistics-pies/peoplepie/peoplepie.component.ts
@@ -1,62 +1,56 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import {Label} from 'ng2-charts';
-import {Chart} from 'chart.js';
+import { Label } from "ng2-charts";
+import { Chart } from "chart.js";
 
-import {StatisticsService} from './../../../../Services/statistics.service';
-import {Peoplepie} from './../models/peoplepie.model';
+import { StatisticsService } from "./../../../../Services/statistics.service";
+import { Peoplepie } from "./../models/peoplepie.model";
+import { ItineraryId, Itineraries } from "src/app/Models/exercise.model";
 
 @Component({
-  selector: 'app-peoplepie',
-  templateUrl: './peoplepie.component.html',
-  styleUrls: ['./peoplepie.component.scss']
+  selector: "app-peoplepie",
+  templateUrl: "./peoplepie.component.html",
+  styleUrls: ["./peoplepie.component.scss"],
 })
 export class PeoplepieComponent implements OnInit {
-
   public pieChartLabels: Label[] = [];
-  public pieChartData: number [] = [];
+  public pieChartData: number[] = [];
 
   constructor(private statisticsService: StatisticsService) {}
 
   ngOnInit() {
-    this.statisticsService.get_itineraryStudents()
-    .subscribe((result: Peoplepie[])=>{
-        result.forEach(element => {
-          this.pieChartData.push(element.students);
-          this.pieChartLabels.push(element.itinerary);
+    this.statisticsService
+      .get_itineraryStudents()
+      .subscribe((result: Peoplepie[]) => {
+        result.forEach((element) => {
+          if ((!this.pieChartData[2]) && (!this.pieChartLabels[2])) {
+            this.pieChartData.push(element.students);
+            this.pieChartLabels.push(element.itinerary);
+          }
         });
-        // To delete the last position of the each of two array options because the common itinerary now is a preamble and it has not sense
-        this.pieChartData.pop();
-        this.pieChartLabels.pop();
-        
-        new Chart('peoplechart', {
-          type: 'pie',
+
+        new Chart("peoplechart", {
+          type: "pie",
           data: {
             labels: this.pieChartLabels,
             datasets: [
               {
                 data: this.pieChartData,
-                backgroundColor: [  
-                  "#ffa3b7",  
-                  "#5cc8f5",  
-                  "#ffe397"
-                ],  
-                fill: true
-              }
-            ]
+                backgroundColor: ["#ffa3b7", "#5cc8f5", "#ffe397"],
+                fill: true,
+              },
+            ],
           },
           options: {
-              legend: {
-                display: true,
-                labels: {
-                  boxWidth: 24,
-                }
+            legend: {
+              display: true,
+              labels: {
+                boxWidth: 24,
               },
-              responsive: true,
             },
+            responsive: true,
+          },
         });
-      }
-    )
+      });
   }
-
 }
